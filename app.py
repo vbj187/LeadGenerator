707016c6f10f99e9ae5d605460c703da3929c865
VIgneshwaran Balamurugan <vigneshwaran.b@exeterpremedia.com>
2020-07-10 10:35:25 +0530

    scraped EContent page

---
+++ app.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+URL = 'http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.html'
+
+response = requests.get(URL)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup)
